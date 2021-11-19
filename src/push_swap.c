e30ca2cdbf3122916a7e1078724cc3cabf3e46fa
Alfredo Braule <alfredoneto934@gmail.com>
2021-11-19 18:13:55 -0300

    receiving arguments from stdin and printing them out

--- src/push_swap.c
+++ src/push_swap.c
@@ -3,16 +3,27 @@
 int main(int argc, char const **argv)
 {
     char *stack;
-    
+    int i;
+    int j;
+
+    i = 0;
+    j = 1;
     stack = (char *)malloc((strlen(*argv) - 1) * sizeof(char));
     if (argc > 1)    
     {
         while (*argv)
         {
-            ft_strlcpy(stack, *argv, sizeof(stack));
+            if(j > 1)
+                stack[i++] = **argv;
             argv++;
-            printf("%c", *stack);
+            j++;
         }
+        while (*stack)
+        {
+            printf("%c ", *stack);
+            stack++;
+        }
+        printf("\n");
     }
     else
         puts("Número inválido de argumentos!");
